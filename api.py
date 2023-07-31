1ff4be300a48475bf4a94feb0ef8213126639231
ReyesTong <54979713+ReyesTong@users.noreply.github.com>
2023-07-31 20:59:50 +0100

    Add files via upload

--- api.py
+++ api.py
@@ -45,28 +45,33 @@ def verify(n: int, digest: bytes, s: int, u: bytes) -> bool:
 
 
 bit_length = 1536  # 指定位数
+""""
 left_privKey = sympy.randprime(2**(bit_length-1), 2**bit_length - 1)
 while left_privKey % 4 != 3:
     left_privKey = sympy.randprime(2**(bit_length-1), 2**bit_length - 1)
 right_privKey = sympy.randprime(2**(bit_length-1), 2**bit_length - 1)
 while right_privKey % 4 != 3:
     right_privKey = sympy.randprime(2**(bit_length-1), 2**bit_length - 1)
-print(left_privKey)
-print(right_privKey)
-pubKey = left_privKey * right_privKey
-print(pubKey)
-msg = 'Hello World!'
-print(msg)
-d_msg = hashlib.sha256(msg.encode('utf-8')).digest()
-print("d-msg finish!")
-sig_s, sig_u = sign(left_privKey, right_privKey, d_msg)
-print(sig_s)
-print(hexlify(sig_u))
-print(verify(pubKey, d_msg, sig_s, sig_u))
 
-print(type(d_msg))
-print(type(sig_s))
-print(type(sig_u))
+"""
+left_privKey = 2156080367722899569999731053453720358158738117017616683882854117204203997378817937284926510479436880215828737085540130441104194724693365379491081914649522326113766759725215373118370592243095071694372728205983601387135121618020530467958335925164512144141818569480084077223911591251585891634203256678214657784010658379966523711311006255131453186089554730058167784424513570695174824923277733401848309182051914509890770698626490876351161830336248987398731951858793163
+right_privKey = 1675870484941349616080095187743192261994904021570345583606005913139243357855366776949260184923578051833370077311269301323089375199771191926474223614757357710385131174647689175863115170231367505670541112817232943446992855517130410535240918860280608278456154660429542210971199394631410104353791272169727799001404808725528918278727786541660685348533508675353773671048763433280283920020723025344742622011381008847209174610598655211643554502659273997424991582428377927
+#print(left_privKey)
+#print(right_privKey)
+pubKey = left_privKey * right_privKey
+#print(pubKey)
+msg = 'Hello World!'
+#print(msg)
+d_msg = hashlib.sha256(msg.encode('utf-8')).digest()
+#print("d-msg finish!")
+sig_s, sig_u = sign(left_privKey, right_privKey, d_msg)
+#print(sig_s)
+#print(hexlify(sig_u))
+#print(verify(pubKey, d_msg, sig_s, sig_u))
+
+#print(type(d_msg))
+#print(type(sig_s))
+#print(type(sig_u))
 
 file_name = 'student_data.csv'
 def read_csv(file_name):
@@ -84,7 +89,7 @@ app = Flask(__name__)
 data = read_csv('student_data.csv')
 
 @app.route('/api/gpa', methods=['GET'])
-def get_gpa():
+def get_gpa(): 
     id = request.args.get('name')
     processed_id = id.replace('_', ' ')
     if processed_id in data:
@@ -96,6 +101,7 @@ def get_gpa():
             'GPA':gpa,
 
             'Digest':d_gpa.hex(),
+            
             'Signatures':{
                 'Rabin':{
                     'PubKey':pubKey,
@@ -113,3 +119,5 @@ if __name__ == '__main__':
     #Test Example:
     #Rachel Williams 1.8
     #http://localhost:5000/api/gpa?name=Rachel_Williams
+    #http://16.171.36.57:5000/api/gpa?name=Rachel_Williams
+    #http://ec2-16-171-36-57.eu-north-1.compute.amazonaws.com:5000/api/gpa?name=Rachel_Williams
